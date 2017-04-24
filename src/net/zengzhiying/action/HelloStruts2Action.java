25a3d89534f39555cb00d40c59bcc532fb5b78df
jacky <1431326613@qq.com>
2017-04-24 18:43:56 +0800

    first

---
+++ src/net/zengzhiying/action/HelloStruts2Action.java
@@ -0,0 +1,22 @@
+package net.zengzhiying.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class HelloStruts2Action  extends ActionSupport {
+
+	
+	@Override
+    public String execute() throws Exception {
+        System.out.println("执行Action");
+        
+        return SUCCESS;
+    }
+    
+    
+    public String login() {
+        return SUCCESS;
+    }
+
+	
+	
+}
